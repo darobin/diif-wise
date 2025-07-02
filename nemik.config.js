3fb032d713283655950d2994f8f90d654abdb68f
Robin Berjon <robin.berjon@nytimes.com>
2025-07-02 19:04:31 +0200

    latest

--- nemik.config.js
+++ nemik.config.js
@@ -5,6 +5,7 @@ export default async function (nemik) {
     .warn(`Processing…`)
     .gdoc('gdoc-rb', '1RNejJAwu0NzAGeVqR2wapLBmjd7sQWCCrkwzu8uoLxA', { save: 'diif-wise.json' })
     .gdoc2html()
+    .bibliography()
     .theme('supramundane')
     .wordCount(40_000)
     .saveHTML({ to: 'diif-wise.html', pretty: true })
