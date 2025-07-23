96f80a7ea783db3445e72f18bffbf7fc2a35ec83
Robin Berjon <robin.berjon@nytimes.com>
2025-07-23 18:31:21 +0200

    starting to look pretty

--- nemik.config.js
+++ nemik.config.js
@@ -6,8 +6,13 @@ export default async function (nemik) {
     .gdoc('gdoc-rb', '1RNejJAwu0NzAGeVqR2wapLBmjd7sQWCCrkwzu8uoLxA', { save: 'diif-wise.json' })
     .gdoc2html()
     .bibliography()
-    .theme('supramundane')
+    .theme('supramundane', {
+      cover: 'ernst-haeckel.png',
+      date: true,
+      author: 'Robin Berjon',
+    })
     .wordCount(40_000)
+    .pdf('diif-wise.pdf')
     .saveHTML({ to: 'diif-wise.html', pretty: true })
   ;
 }
